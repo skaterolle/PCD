17a4fff7287d2e79d03e92c5b0709c6bedcf3411
skaterolle <albertroider@hotmail.com>
2018-01-09 11:36:40 +0100

    Update Almacen.java

--- SolucionG1/Proyecto3/src/proyecto3/Almacen.java
+++ SolucionG1/Proyecto3/src/proyecto3/Almacen.java
@@ -17,27 +17,27 @@ public class Almacen {
     private int fruta;
 
     public Almacen(int _capacidad, int _fruta) {
-        capacidad = _capacidad;
-        fruta = _fruta;
+        capacidad = _capacidad; // capacidad total del almacen
+        fruta = _fruta;	// Fruta que hay en estos momentos en el almacen, no puede ser superior a la capacidad
     }
 
     public synchronized void Deposita(int id, int cuanto) {
         try {
             while (capacidad - fruta < cuanto) {
-                wait();
+                wait(); //Como no tenemos mutex se hacen esperas de los propios hilos
             }
             fruta = fruta + cuanto;
             System.out.println("CONTENIDO: " + fruta);
-            notifyAll();
+            notifyAll(); //Despertamos a todos los hilos que estan esperando
         } catch (InterruptedException ex) {
             System.out.println(id + "No ha podido entregar la fruta");
-            System.exit(0);
+            System.exit(0); //salimos directamente y cierra el hilo
         }
     }
 
     public synchronized void Compra(int id, int cuanto) {
         try {
-            while (fruta < cuanto) {
+            while (fruta < cuanto) { //Si la cantidad pedida es superior a la contenida se espera
                 wait();
             }
             fruta = fruta - cuanto;
